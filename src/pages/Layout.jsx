efe73ba8671cff1701dc44b8b1d5deed75a9b585
lluciferr <n.gya054@gmail.com>
2023-06-17 16:14:18 +0545

    messenger plugin added

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,10 +5,19 @@ import Team from "../components/team/Team";
 import Footer from "../components/footer/Footer";
 import Testimony from "../components/testimony/testimony";
 
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+
 function Layout() {
   return (
     <>
     {/* <h1>hooo</h1> */}
+    <MessengerCustomerChat
+    		pageId="106947385769745"
+        appId="246863197967981"
+    
+    />
+
       <Home />
 		<About/>	
     <Events/>
