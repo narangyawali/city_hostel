30c8b8dd2abac54fb0ef6761f6a40623db441a7d
lluciferr <n.gya054@gmail.com>
2023-06-18 13:31:51 +0545

    mobile overflowX issue fixed

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import Home from './components/home/Home.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
+    <div style={{overflowX:"hidden"}}>
   	<Layout/>
+    </div>
   </React.StrictMode>,
 )
